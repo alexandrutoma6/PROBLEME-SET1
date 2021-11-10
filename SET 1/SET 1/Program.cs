07fe5857680501e4c75ab9de8d6d39958317c694
alexandrutoma6 <a_tomaaa@yahoo.com>
2021-11-10 23:11:37 +0200

    set 1 update

--- SET 1/SET 1/Program.cs
+++ SET 1/SET 1/Program.cs
@@ -10,42 +10,74 @@ namespace SET_1
     {
         static void Main(string[] args)
         {
-            //p1();
-            //p2();
-            //p3();
-            //p4();
-            //p5();
-            //p6();
-            //p7();
-            //p8();
-            //p9();
-            //p10();
-            //p11();
-            //p12();
-            //p13();
-            //p14();
-            //p15();
-            //p16();
-            //p17();
-            p18();
+            Console.WriteLine("1. Rezolvati ecuatia de gradul 1 cu o necunoscuta: ax+b = 0, unde a si b sunt date de intrare. ");
+            Console.WriteLine("2. Rezolvati ecuatia de gradul 2 cu o necunoscuta: ax^2 + bx + c = 0, unde a, b si c sunt date de intrare. Tratati toate cazurile posibile. ");
+            Console.WriteLine("3. Determinati daca n se divide cu k, unde n si k sunt date de intrare. ");
+            Console.WriteLine("4. Detreminati daca un an y este an bisect. ");
+            Console.WriteLine("5. Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga. ");
+            Console.WriteLine("6. Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. ");
+            Console.WriteLine("7. (Swap) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor. ");
+            Console.WriteLine("8. (Swap restrictionat) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare.  ");
+            Console.WriteLine("9. Afisati toti divizorii numarului n. ");
+            Console.WriteLine("10. Test de primalitate: determinati daca un numar n este prim.");
+            Console.WriteLine("11. Afisati in ordine inversa cifrele unui numar n. ");
+            Console.WriteLine("12. Determinati cate numere integi divizibile cu n se afla in intervalul [a, b]. ");
+            Console.WriteLine("13. Determianti cati ani bisecti sunt intre anii y1 si y2.");
+            Console.WriteLine("14. Determianti daca un numar n este palindrom. (un numar este palindrom daca citit invers obtinem un numar egal cu el, de ex. 121 sau 12321. ");
+            Console.WriteLine("15. Se dau 3 numere. Sa se afiseze in ordine crescatoare. ");
+            Console.WriteLine("16. Se dau 5 numere. Sa se afiseze in ordine crescatoare. (nu folositi tablouri)");
+            Console.WriteLine("17. Determianti cel mai mare divizor comun si cel mai mic multiplu comun a doua numere. Folositi algoritmul lui Euclid.");
+            Console.WriteLine("18. Afisati descompunerea in factori primi ai unui numar n.  De ex. pentru n = 1776 afisati 2^3 x 3^1 x 7^2. ");
+            Console.WriteLine("19. Determinati daca un numar e format doar cu 2 cifre care se pot repeta. De ex. 23222 sau 9009000 sunt astfel de numere, pe cand 593 si 4022 nu sunt. ");
+            Console.WriteLine("20. Afisati fractia m/n in format zecimal, cu perioada intre paranteze (daca e cazul). Exemplu: 13/30 = 0.4(3).");
+            Console.WriteLine("21. Ghiciti un numar intre 1 si 1024 prin intrebari de forma ``numarul este mai mare sau egal decat x ?``.");
+            Console.WriteLine();
+            Console.Write("ALEGETI PROBLEMA NUMARUL: ");
+            int problema = int.Parse(Console.ReadLine());
 
-        }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
 
-        /// <summary>
-        ///Afisati descompunerea in factori primi ai unui numar n.  De ex. pentru n = 1776 afisati 2^3 x 3^1 x 7^2.  
-        /// </summary>
-        private static void p18()
-        {
-            int[] factor= 0;
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 2; i <= n/2; i++)
-            {
-                if (n % i == 0)
-                {
-                    n = n / i;
-                    factor[i]++;
-                }
-            }
+            if (problema == 1)
+                p1();
+            if (problema == 2)
+                p2();
+            if (problema == 3)
+                p3();
+            if (problema == 4)
+                p4();
+            if (problema == 5)
+                p5();
+            if (problema == 6)
+                p6();
+            if (problema == 7)
+                p7();
+            if (problema == 8)
+                p8();
+            if (problema == 9)
+                p9();
+            if (problema == 10)
+                p10();
+            if (problema == 11)
+                p11();
+            if (problema == 12)
+                p12();
+            if (problema == 13)
+                p13();
+            if (problema == 14)
+                p14();
+            if (problema == 15)
+                p15();
+            if (problema == 16)
+                p16();
+            if (problema == 17)
+                p17();
+
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            Console.WriteLine();
         }
 
         /// <summary>
